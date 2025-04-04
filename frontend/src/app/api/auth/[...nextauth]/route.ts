61279575e94eae16527370274218d3cf94e74e63
Jason Yap <jasonyapzx@gmail.com>
2025-04-04 23:16:38 +0800

    feat: add auth

---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,50 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+
+const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  
+  // Add these configuration options
+  useSecureCookies: process.env.NODE_ENV === "production",
+  cookies: {
+    sessionToken: {
+      name: `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
+
+  providers: [
+    {
+      id: "worldcoin",
+      name: "Worldcoin",
+      type: "oauth",
+      wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
+      authorization: { params: { scope: "openid" } },
+      clientId: process.env.WLD_CLIENT_ID,
+      clientSecret: process.env.WLD_CLIENT_SECRET,
+      idToken: true,
+      checks: ["state", "nonce", "pkce"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.sub,
+          verificationLevel:
+            profile["https://id.worldcoin.org/v1"].verification_level,
+        };
+      },
+    },
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      return true;
+    },
+  },
+  debug: process.env.NODE_ENV === "development",
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
